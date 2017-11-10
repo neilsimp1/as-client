1d3324018795445693d2b737893dcc0839e0b24e
Simpson <Neil.Simpson@its.ny.gov>
2017-11-10 08:22:59 -0500

    Reloading working, iframe size working

--- renderer.js
+++ renderer.js
@@ -1,24 +1,27 @@
 const form = document.querySelector("form");
 
+let iframe = document.createElement("iframe");
+
 window.addEventListener("DOMContentLoaded", function() {
-	const url = window.localStorage.getItem("url")
-	if(url) form.elements.url.value = url;
+	const url = window.localStorage.getItem("url");
+	if(sessionStorage.getItem("isPageLoaded")) loadPage(url);
+	else if(url) form.elements.url.value = url;
 });
 
 form.addEventListener("submit", function() {
 	const url = this.elements.url.value;
-
-	if(/(.*):(\d*)\/?(.*)/.test(url)){
-		window.localStorage.setItem("url", url);
-		//ipcRenderer.sendSync("loadPage", url);
-		let iframe = document.createElement("iframe");
-		iframe.src= url;
-		iframe.width = window.innerWidth;
-		iframe.height = window.innerHeight;
-
-		document.body.innerHTML = "";
-		document.body.appendChild(iframe);
-	}
-
+	if(/(.*):(\d*)\/?(.*)/.test(url)) loadPage(url);
 	return false;
 });
+
+function loadPage(url) {
+	window.localStorage.setItem("url", url);
+
+	iframe.src = url;
+
+	document.body.innerHTML = "";
+	document.body.appendChild(iframe);
+
+	window.document.title = "AudioStreamer";
+	sessionStorage.setItem("isPageLoaded", true);
+}
